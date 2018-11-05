d53c2a8de668366150f4c49de78b7d59eea71125
Ricardo Mirón <r.mirontorres@ugto.mx>
2018-11-05 19:12:30 +0100

    Created Main Class

--- resources/SpeedRadar.java
+++ resources/SpeedRadar.java
@@ -2,50 +2,19 @@ package sisdistr;
 
 import sisdistr.events.TrafficEvent;
 import sisdistr.events.SpeedRadarEvent;
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.api.java.functions.FunctionAnnotation;
-import org.apache.flink.api.java.tuple.Tuple8;
 
 public class SpeedRadar {
 
-    public static void main(String[] args){
-
-        final StreamExecutionEnvironment env =
-                StreamExecutionEnvironment.getExecutionEnvironment();
-        String inFilePath = args[0];
-        String outFilePath = args[1];
-        DataStreamSource<String> source = env.readTextFile(inFilePath);
-
-
-        SingleOutputStreamOperator<TrafficEvent> filterOut = source.map(new MapFunction<String, TrafficEvent>() {
-			TrafficEvent out = new TrafficEvent();
-
-                @Override
-            public TrafficEvent map(String in) throws Exception {
-                String[] fieldArray = in.split(",");
-				out.setTime(Integer.parseInt(fieldArray[0]));
-				out.setVid(Integer.parseInt(fieldArray[1]));
-				out.setSpeed(Integer.parseInt(fieldArray[2]));
-				out.setHighway(Integer.parseInt(fieldArray[3]));
-				out.setLane(Integer.parseInt(fieldArray[4]));
-				out.setDirection(Integer.parseInt(fieldArray[5]));
-				out.setSegment(Integer.parseInt(fieldArray[6]));
-				out.setPosition(Integer.parseInt(fieldArray[7]));
-
-                return out;
-            }
-        }).filter(new FilterFunction<TrafficEvent>() {
+    public static SingleOutputStreamOperator something (SingleOutputStreamOperator<TrafficEvent> tuples){
+      return tuples
+        .filter(new FilterFunction<TrafficEvent>() {
             @Override
             public boolean filter(TrafficEvent in) throws Exception {
                 if(in.getSpeed() >= 90){ return true;
                 }else{return false;}
             }
-        });
-        SingleOutputStreamOperator<SpeedRadarEvent> speedRadarReport = filterOut.map(new MapFunction<TrafficEvent, SpeedRadarEvent>() {
+        })
+        .map(new MapFunction<TrafficEvent, SpeedRadarEvent>() {
 
           SpeedRadarEvent out = new SpeedRadarEvent();
 
@@ -61,14 +30,5 @@ public class SpeedRadar {
             return out;
           }
         });
-
-		 speedRadarReport.writeAsCsv(outFilePath);
-
-        try {
-            env.execute("SpeedRadar");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
